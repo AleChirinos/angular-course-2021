71ccb6e8b6a9d0360f0e6e03b5299772b925dd10
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-08-12 00:04:35 -0400

    08/10/2021 life cycle in Angular

--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -1,19 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test3',
   templateUrl: './test3.component.html',
   styleUrls: ['./test3.component.css']
 })
-export class Test3Component {
+export class Test3Component implements OnInit, OnChanges, DoCheck, OnDestroy, AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked {
 
-  name="alejandra"
-  lastname="chirinos"
+  @Input() name2;
 
-  constructor() { }
+  name="alejandra";
+  lastname="chirinos";
+
+  constructor() {
+    console.log('CONSTRUCTOR');
+  }
+
+  ngOnChanges(changes: any){
+    console.log('ON CHANGES', changes);
+  }
+
+  ngOnInit(){
+    console.log('ON INIT');
+  }
+
+  ngDoCheck(){
+    console.log('DO CHECK');
+  }
+
+  ngOnDestroy(){
+    console.log('ON DESTROY');
+  }
+
+  ngAfterContentInit(){
+    console.log('AFTER CONTENT INIT');
+  }
+
+  ngAfterContentChecked(){
+    console.log('AFTER CONTENT CHECK');
+  }
+
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
+  }
+
+  ngAfterViewChecked(){
+    console.log('AFTER VIEW CHECK');
+  }
 
   test0(event){
-    console.log('event click: ', event);
     console.log('DATA: ', this.name+ ' ' +this.lastname);
   }
 
