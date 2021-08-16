621432f08e1536a40abe84ef8f0d6a3093de9d63
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-08-16 19:05:03 -0400

    16/08/2021 create my first attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -143,4 +143,8 @@ export class AppComponent {
   printData(event) {
     console.log('CHILD COMP SEND DATA: ', event);
   }
+
+  test(event){
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAA', event);
+  }
 }
