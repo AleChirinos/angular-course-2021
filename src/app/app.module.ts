eb6ba88e7e1ead4aacb793ee7267c27f9bf9ccfe
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-08-26 14:49:08 -0400

    25/08/2021 navigation by modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { View1Component } from './view1/view1.component';
+import { View1sub1Component } from './view1/view1sub1/view1sub1.component';
+import { View1sub2Component } from './view1sub2/view1sub2.component';
 import { View2Component } from './view2/view2.component';
 
 const routes : Routes= [
@@ -23,10 +25,23 @@ const routes : Routes= [
     path: '', redirectTo: 'view1', pathMatch: 'full'
   },
   {
-    path: 'view1', component: View1Component
+    path: 'view1', component: View1Component, children: [ 
+      {
+        path: '', redirectTo: 'view1sub1', pathMatch: 'full'
+      },
+      {
+        path: 'view1sub1', component: View1sub1Component
+      },
+      {
+        path: 'view1sub2', component: View1sub2Component
+      }
+    ]
   },
   {
     path: 'view2', component: View2Component
+  },
+  {
+    path: 'view3', loadChildren: () => import('./view3/view3.module').then(m => m.View3Module)
   }
 ];
 
@@ -39,7 +54,9 @@ const routes : Routes= [
     Directive1Directive,
     Directive2Directive,
     View1Component,
-    View2Component
+    View2Component,
+    View1sub1Component,
+    View1sub2Component
   ],
   imports: [
     BrowserModule,
