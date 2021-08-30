508969f9875af5d28002703f5c46e1495bcd19cc
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-08-30 18:47:23 -0400

    30/08/2021 Configuration of Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 const routes : Routes= [
   {
@@ -23,6 +24,7 @@ const routes : Routes= [
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import {NgbButtonsModule, NgbModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 const routes : Routes = [
   {
@@ -18,6 +20,8 @@ const routes : Routes = [
     FormsModule,
     NgbToastModule,
     NgbButtonsModule,
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   declarations: [
     LoginComponent
