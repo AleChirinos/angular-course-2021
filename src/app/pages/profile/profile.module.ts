e238658d3c9403ba06cfd44bd90a95377062fa69
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-09-02 13:54:27 -0400

    configuration with firebase

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import {RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 
 const routes : Routes = [
