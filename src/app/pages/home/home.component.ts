be02c603b3db332f2e5a58d6beb4ea89a5a12eba
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-09-16 03:16:20 -0400

    15/09/2021 task publications in profile and home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,6 +8,8 @@ import {HomeService} from "./home.service";
 })
 export class HomeComponent implements OnInit {
 
+  posts:any[] = [];
+
   constructor(private publicationService: PublicationService,
               private homeService: HomeService) { }
 
@@ -27,7 +29,8 @@ export class HomeComponent implements OnInit {
   loadData(){
     this.publicationService.getAll().subscribe(res => {
 
-      console.log('RES PUBLICATIONS', res);
+      this.posts = Object.entries(res).map((s: any) => ({id: s[0], ...s[1]}));
+      console.log(res);
 
     })
   }
--- src/app/shared/modules/message-sender/components/post-message/post-message.component.ts
+++ src/app/shared/modules/message-sender/components/post-message/post-message.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from "../../../../../core/services/auth.service";
 import {HomeService} from "../../../../../pages/home/home.service";
 import {MatDialogRef} from "@angular/material/dialog";
 import {PublicationService} from "../../../../../pages/shared/services/publication.service";
+import {ProfileService} from "../../../../../pages/profile/profile.service";
 
 @Component({
   selector: 'app-post-message',
@@ -14,7 +15,8 @@ export class PostMessageComponent implements OnInit {
   constructor(private authService: AuthService,
               private publicationService: PublicationService,
               private homeService: HomeService,
-              public dialogRef: MatDialogRef<PostMessageComponent>) { }
+              public dialogRef: MatDialogRef<PostMessageComponent>,
+              private profileService:ProfileService) { }
 
   ngOnInit(): void {
   }
@@ -26,6 +28,8 @@ export class PostMessageComponent implements OnInit {
       idUser: this.authService.getUserId()
     }).subscribe( res => {
       this.homeService.setLoad(true);
+      this.profileService.setLoad(true);
+      this.dialogRef.close();
     })
 
   }
