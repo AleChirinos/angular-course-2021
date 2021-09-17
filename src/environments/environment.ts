f8358ba13497339e0029dd548a4ad7af7ae7ac84
AleChirinos <alejandra.chirinos2702@gmail.com>
2021-09-16 20:20:13 -0400

    Finish final test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   app: {
-    apiBaseUrl: "https://data-test2-b1af6-default-rtdb.firebaseio.com/"
+    apiBaseUrl: "https://bitcoin-test-a0874-default-rtdb.firebaseio.com/"
   }
 };
 
